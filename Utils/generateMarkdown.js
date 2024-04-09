b3182511ac79b79616953c6a8f244a276587432e
Piattie <158129594+Piattie@users.noreply.github.com>
2024-04-08 22:12:07 -0700

    Draft 4

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -1,45 +1,85 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (!license) return '';
+    if (!license || license === 'None') return '';
     return `![License](https://img.shields.io/badge/license-${encodeURIComponent(license)}-blue.svg)`;
-  }
+}
   
-  // TODO: Create a function that returns the license link
-  // If there is no license, return an empty string
-  function renderLicenseLink(license) {
-    if (!license) return '';
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+    if (!license || license === 'None') return '';
     return `https://choosealicense.com/licenses/${encodeURIComponent(license).toLowerCase()}/`;
-  }
-  
-  // TODO: Create a function that returns the license section of README
-  // If there is no license, return an empty string
-  function renderLicenseSection(license) {
-    if (!license) return '';
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+    if (!license || license === 'None') return '';
     return `
-  ## License
-  
-  This project is licensed under the ${license} license. For more information, see [here](${renderLicenseLink(license)}).
-  `;
-  }
-  
-  // TODO: Create a function to generate markdown for README
-  function generateMarkdown(data) {
+## License
+
+This project is licensed under the ${license} license. For more information, see [here](${renderLicenseLink(license)}).
+`;
+}
+
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
     return `# ${data.title}
-  ${renderLicenseBadge(data.license)}
-  
-  ## Description
-  ${data.description}
-  
-  // Add other sections here...
-  
-  ${renderLicenseSection(data.license)}
-  
-  ## Questions
-  For questions, you can reach me at [${data.github}](https://github.com/${data.github}) or via email at ${data.email}.
-  `;
-  }
-  
-  
-  module.exports = generateMarkdown;
-  
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Motivation
+${data.motivation}
+
+## Problem Solved
+${data.problem}
+
+## What I Learned
+${data.learning}
+
+## Installation
+${data.installation}
+
+## System Requirements
+${data.requirements ? data.requirements : 'No specific system requirements or dependencies.'}
+
+## Version Requirements
+${data.version ? data.version : 'No specific version requirements.'}
+
+## Usage
+${data.usage}
+
+## Commands
+${data.commands ? data.commands : 'No specific additional commands required.'}
+
+## Examples
+${data.examples ? data.examples : 'No examples provided.'}
+
+${data.includeScreenshots ? `## Screenshots\n![Screenshot](${data.screenshotPath})\n` : ''}
+
+## Credits
+${data.credits ? data.credits : 'No additional credits.'}
+
+## Third-Party Assets
+${data.assets ? data.assets : 'No third-party assets used.'}
+
+${data.features ? `## Features\n${data.features}\n` : ''}
+
+${data.contributing ? `## Contributing\n${data.contributionGuidelines}\n` : ''}
+
+${data.tests ? `## Tests\nTo run tests, use the following command:\n\`\`\`\n${data.testInstructions}\n\`\`\`\n` : ''}
+
+${renderLicenseSection(data.license)}
+
+## Additional Information
+${data.additionalInfo ? data.additionalInfo : 'No additional information provided.'}
+
+## Questions
+For questions, you can reach me at [${data.github}](https://github.com/${data.github}) or via email at ${data.email}.
+`;
+};
+
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -6,136 +6,168 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: input => input.trim() !== '' ? true : 'Project title cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'description',
-      message: 'What is the main purpose of your project?'
+        type: 'input',
+        name: 'description',
+        message: 'What is the main purpose of your project?',
+        validate: input => input.trim() !== '' ? true : 'Description cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'motivation',
-      message: 'What was your motivation for starting this project?'
+        type: 'input',
+        name: 'motivation',
+        message: 'What was your motivation for starting this project?',
+        validate: input => input.trim() !== '' ? true : 'Motivation cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'problem',
-      message: 'What problem does your project solve?'
+        type: 'input',
+        name: 'problem',
+        message: 'What problem does your project solve?',
+        validate: input => input.trim() !== '' ? true : 'Problem description cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'learning',
-      message: 'What did you learn while working on this project?'
+        type: 'input',
+        name: 'learning',
+        message: 'What did you learn while working on this project?',
+        validate: input => input.trim() !== '' ? true : 'Learning cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'installation',
-      message: 'What are the steps to install your project?'
+        type: 'input',
+        name: 'installation',
+        message: 'What are the steps to install your project?',
+        validate: input => input.trim() !== '' ? true : 'Installation steps cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'requirements',
-      message: 'Are there any system requirements or dependencies?'
+        type: 'input',
+        name: 'requirements',
+        message: 'Are there any system requirements or dependencies?',
+        validate: input => input.trim() !== '' ? true : 'Requirements cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'version',
-      message: 'Are there any specific version requirements for software or libraries?'
+        type: 'input',
+        name: 'version',
+        message: 'Are there any specific version requirements for software or libraries?',
     },
     {
-      type: 'input',
-      name: 'usage',
-      message: 'How is your project used?'
+        type: 'input',
+        name: 'usage',
+        message: 'How is your project used?',
+        validate: input => input.trim() !== '' ? true : 'Usage cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'commands',
-      message: 'Are there any special commands or steps the user should know about?'
+        type: 'input',
+        name: 'commands',
+        message: 'Are there any special commands or steps the user should know about?',
     },
     {
-      type: 'input',
-      name: 'examples',
-      message: 'Can you provide examples or use cases?'
+        type: 'input',
+        name: 'examples',
+        message: 'Can you provide examples or use cases?',
     },
     {
-      type: 'confirm',
-      name: 'screenshots',
-      message: 'Would you like to include screenshots?'
+        type: 'confirm',
+        name: 'includeScreenshots',
+        message: 'Would you like to include screenshots?'
     },
     {
-      type: 'input',
-      name: 'credits',
-      message: 'Did you collaborate with others on this project? If so, who are they?'
+        type: 'input',
+        name: 'screenshotPath',
+        message: 'Enter the relative path to your screenshots:',
+        when: answers => answers.includeScreenshots,
+        validate: input => input.trim() !== '' ? true : 'Please provide a path for the screenshots.'
     },
     {
-      type: 'input',
-      name: 'assets',
-      message: 'Did you use any third-party assets or follow any tutorials? Please provide details.'
+        type: 'input',
+        name: 'credits',
+        message: 'Did you collaborate with others on this project? If so, who are they?'
     },
     {
-      type: 'list',
-      name: 'license',
-      message: 'Which license do you want to apply to your project?',
-      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+        type: 'input',
+        name: 'assets',
+        message: 'Did you use any third-party assets or follow any tutorials? Please provide details.'
     },
     {
-      type: 'input',
-      name: 'badges',
-      message: 'Are there any specific badges you want to include?'
+        type: 'list',
+        name: 'license',
+        message: 'Which license do you want to apply to your project?',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
     },
     {
-      type: 'input',
-      name: 'features',
-      message: 'What are the key features of your project?'
+        type: 'input',
+        name: 'badges',
+        message: 'Are there any specific badges you want to include?'
     },
     {
-      type: 'confirm',
-      name: 'contributing',
-      message: 'Do you want to include guidelines for how others can contribute to your project?'
+        type: 'input',
+        name: 'features',
+        message: 'What are the key features of your project?',
+        validate: input => input.trim() !== '' ? true : 'Features cannot be empty.'
     },
     {
-      type: 'list',
-      name: 'contributionGuidelines',
-      message: 'Would you like to use a standard contribution guideline or write custom guidelines?',
-      choices: ['Standard', 'Custom']
+        type: 'confirm',
+        name: 'contributing',
+        message: 'Do you want to include guidelines for how others can contribute to your project?'
     },
     {
-      type: 'confirm',
-      name: 'tests',
-      message: 'Do you have any tests for your project?'
+        type: 'input',
+        name: 'contributionGuidelines',
+        message: 'Provide your contribution guidelines:',
+        when: answers => answers.contributing,
+        validate: input => input.trim() !== '' ? true : 'Contribution guidelines cannot be empty.'
     },
     {
-      type: 'input',
-      name: 'testInstructions',
-      message: 'How can these tests be run?'
+        type: 'confirm',
+        name: 'tests',
+        message: 'Do you have any tests for your project?'
     },
     {
-      type: 'input',
-      name: 'additionalInfo',
-      message: 'Is there any other information you\'d like to include in the README that hasn\'t been covered yet?'
-    }
-  ];
+        type: 'input',
+        name: 'testInstructions',
+        message: 'How can these tests be run?',
+        when: answers => answers.tests,
+        validate: input => input.trim() !== '' ? true : 'Test instructions cannot be empty.'
+    },
+    {
+        type: 'input',
+        name: 'additionalInfo',
+        message: 'Is there any other information you\'d like to include in the README that hasn\'t been covered yet?'
+    },
+    {
+        type: 'input',
+        name: 'fileName',
+        message: 'What would you like to name your README file?',
+        default: 'README.md',
+        validate: input => input.trim() !== '' ? true : 'Filename cannot be empty.'
+    },
+];
   
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-      err ? console.log(err) : console.log('Successfully created README.md!')
-    );
-  }
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+            return;
+        }
+        console.log(`Successfully created ${fileName}!`);
+    });
+}
+
   
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
-      .then((answers) => {
-        const readmeContent = generateMarkdown(answers);
-        writeToFile('README.md', readmeContent);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
+        .then(answers => {
+            const readmeContent = generateMarkdown(answers);
+            writeToFile(answers.fileName, readmeContent);
+        })
+        .catch(error => {
+            console.error('An error occurred during the interview:', error);
+        });
+}
+
 // Function call to initialize app
 init();
