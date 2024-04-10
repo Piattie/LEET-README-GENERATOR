3af72cab391e4475bd28434c68e4c2f37e6c1cd8
Piattie <158129594+Piattie@users.noreply.github.com>
2024-04-09 22:46:07 -0700

    Draft 5

--- Utils/generateMarkdown.js
+++ Utils/generateMarkdown.js
@@ -12,17 +12,6 @@ function renderLicenseLink(license) {
     return `https://choosealicense.com/licenses/${encodeURIComponent(license).toLowerCase()}/`;
 }
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-    if (!license || license === 'None') return '';
-    return `
-## License
-
-This project is licensed under the ${license} license. For more information, see [here](${renderLicenseLink(license)}).
-`;
-}
-
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
     return `# ${data.title}
@@ -31,55 +20,36 @@ ${renderLicenseBadge(data.license)}
 ## Description
 ${data.description}
 
-## Motivation
-${data.motivation}
-
-## Problem Solved
-${data.problem}
-
-## What I Learned
-${data.learning}
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contribution](#contribution)
+- [Tests](#tests)
+- [License](#license)
+- [Questions](#questions)
 
 ## Installation
 ${data.installation}
 
-## System Requirements
-${data.requirements ? data.requirements : 'No specific system requirements or dependencies.'}
-
-## Version Requirements
-${data.version ? data.version : 'No specific version requirements.'}
-
 ## Usage
 ${data.usage}
+${data.includeScreenshots ? `![Screenshot](${data.screenshotPath})` : ''}
 
-## Commands
-${data.commands ? data.commands : 'No specific additional commands required.'}
+## Contribution
+${data.contribution ? data.contributionGuidelines : 'N/A'}
 
-## Examples
-${data.examples ? data.examples : 'No examples provided.'}
+## Tests
+${data.tests ? data.testInstructions : 'N/A'}
 
-${data.includeScreenshots ? `## Screenshots\n![Screenshot](${data.screenshotPath})\n` : ''}
-
-## Credits
-${data.credits ? data.credits : 'No additional credits.'}
-
-## Third-Party Assets
-${data.assets ? data.assets : 'No third-party assets used.'}
-
-${data.features ? `## Features\n${data.features}\n` : ''}
-
-${data.contributing ? `## Contributing\n${data.contributionGuidelines}\n` : ''}
-
-${data.tests ? `## Tests\nTo run tests, use the following command:\n\`\`\`\n${data.testInstructions}\n\`\`\`\n` : ''}
-
-${renderLicenseSection(data.license)}
-
-## Additional Information
-${data.additionalInfo ? data.additionalInfo : 'No additional information provided.'}
+## License
+This project is licensed under the ${data.license} license. [License Link](${renderLicenseLink(data.license)})
 
 ## Questions
-For questions, you can reach me at [${data.github}](https://github.com/${data.github}) or via email at ${data.email}.
-`;
-};
+For any questions, please contact me at [${data.email}](mailto:${data.email}).
+
+GitHub Profile: [${data.githubUsername}](https://github.com/${data.githubUsername})
+    `;
+}
 
 module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
+        name: 'fileName',
+        message: 'What would you like to name your README file?',
+        default: 'README.md',
+        validate: input => input.trim() !== '' ? true : 'Filename cannot be empty.'
+    },
+    {
+        type: 'input',
         name: 'title',
         message: 'What is the title of your project?',
         validate: input => input.trim() !== '' ? true : 'Project title cannot be empty.'
@@ -19,54 +26,15 @@ const questions = [
     },
     {
         type: 'input',
-        name: 'motivation',
-        message: 'What was your motivation for starting this project?',
-        validate: input => input.trim() !== '' ? true : 'Motivation cannot be empty.'
-    },
-    {
-        type: 'input',
-        name: 'problem',
-        message: 'What problem does your project solve?',
-        validate: input => input.trim() !== '' ? true : 'Problem description cannot be empty.'
-    },
-    {
-        type: 'input',
-        name: 'learning',
-        message: 'What did you learn while working on this project?',
-        validate: input => input.trim() !== '' ? true : 'Learning cannot be empty.'
-    },
-    {
-        type: 'input',
         name: 'installation',
         message: 'What are the steps to install your project?',
         validate: input => input.trim() !== '' ? true : 'Installation steps cannot be empty.'
     },
     {
         type: 'input',
-        name: 'requirements',
-        message: 'Are there any system requirements or dependencies?',
-        validate: input => input.trim() !== '' ? true : 'Requirements cannot be empty.'
-    },
-    {
-        type: 'input',
-        name: 'version',
-        message: 'Are there any specific version requirements for software or libraries?',
-    },
-    {
-        type: 'input',
         name: 'usage',
         message: 'How is your project used?',
         validate: input => input.trim() !== '' ? true : 'Usage cannot be empty.'
-    },
-    {
-        type: 'input',
-        name: 'commands',
-        message: 'Are there any special commands or steps the user should know about?',
-    },
-    {
-        type: 'input',
-        name: 'examples',
-        message: 'Can you provide examples or use cases?',
     },
     {
         type: 'confirm',
@@ -81,42 +49,15 @@ const questions = [
         validate: input => input.trim() !== '' ? true : 'Please provide a path for the screenshots.'
     },
     {
-        type: 'input',
-        name: 'credits',
-        message: 'Did you collaborate with others on this project? If so, who are they?'
-    },
-    {
-        type: 'input',
-        name: 'assets',
-        message: 'Did you use any third-party assets or follow any tutorials? Please provide details.'
-    },
-    {
-        type: 'list',
-        name: 'license',
-        message: 'Which license do you want to apply to your project?',
-        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
-    },
-    {
-        type: 'input',
-        name: 'badges',
-        message: 'Are there any specific badges you want to include?'
-    },
-    {
-        type: 'input',
-        name: 'features',
-        message: 'What are the key features of your project?',
-        validate: input => input.trim() !== '' ? true : 'Features cannot be empty.'
-    },
-    {
         type: 'confirm',
-        name: 'contributing',
+        name: 'contribution',
         message: 'Do you want to include guidelines for how others can contribute to your project?'
     },
     {
         type: 'input',
         name: 'contributionGuidelines',
         message: 'Provide your contribution guidelines:',
-        when: answers => answers.contributing,
+        when: answers => answers.contribution,
         validate: input => input.trim() !== '' ? true : 'Contribution guidelines cannot be empty.'
     },
     {
@@ -132,16 +73,22 @@ const questions = [
         validate: input => input.trim() !== '' ? true : 'Test instructions cannot be empty.'
     },
     {
-        type: 'input',
-        name: 'additionalInfo',
-        message: 'Is there any other information you\'d like to include in the README that hasn\'t been covered yet?'
+        type: 'list',
+        name: 'license',
+        message: 'Which license do you want to apply to your project?',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
     },
     {
         type: 'input',
-        name: 'fileName',
-        message: 'What would you like to name your README file?',
-        default: 'README.md',
-        validate: input => input.trim() !== '' ? true : 'Filename cannot be empty.'
+        name: 'githubUsername',
+        message: 'Enter your GitHub username:',
+        validate: input => input.trim() !== '' ? true : 'GitHub username cannot be empty.'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+        validate: input => input.trim() !== '' ? true : 'Email address cannot be empty.'
     },
 ];
   
